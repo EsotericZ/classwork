0d7028ffdf24620445ca64dd21b973285e43c0d3
EsotericZ <cjsand03@gmail.com>
2022-01-18 20:45:47 -0700

    updated

--- week12/mysqlTodos/config/index.js
+++ week12/mysqlTodos/config/index.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'password',
-    database: 'toods_db'
+    database: 'todos_db'
 }).promise();
 
 module.exports = connection;
--- week12/mysqlTodos/server.js
+++ week12/mysqlTodos/server.js
@@ -8,4 +8,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.post('/api/todos', async (req, res) => {
+    const { task } = req.body;
+    if (!task) {
+        return res.status(400).json({error: 'You must provide a task'});
+    }
+    try {
+        const insertQuery = 'INSERT INTO todos (task) VALUES (?);';
+        const getTodoById = 'SELECT * FROM todos WHERE id = ?;';
+        const [result] = await connection.query(insertQuery, [task]);
+        const [todosResult] = await connection.query(getTodoById, [result.insertId]);
+        res.json(todosResult[0]);
+    } catch (e) {
+        res.status(400).json(e);
+    }
+})
+
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
