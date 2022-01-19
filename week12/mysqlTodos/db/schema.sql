a1b1ade2ae08faeb476a4795e3e00985bc4ab4ed
EsotericZ <cjsand03@gmail.com>
2022-01-18 21:41:25 -0700

    added users table

--- week12/mysqlTodos/db/schema.sql
+++ week12/mysqlTodos/db/schema.sql
@@ -2,10 +2,18 @@ DROP DATABASE IF EXISTS todos_db;
 CREATE DATABASE todos_db;
 USE todos_db;
 
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE todos (
     id INT NOT NULL AUTO_INCREMENT,
     task VARCHAR(30) NOT NULL,
     completed BOOLEAN DEFAULT FALSE,
     date_created DATETIME DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id)
+    userId INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (userId) REFERENCES users (id) ON DELETE SET NULL
 );
