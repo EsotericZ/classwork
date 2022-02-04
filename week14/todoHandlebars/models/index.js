46c9fc9e3cc8bf07aea6ea20002a07c85eae0977
EsotericZ <cjsand03@gmail.com>
2022-02-03 19:31:28 -0700

    fix

--- week14/todoHandlebars/models/index.js
+++ week14/todoHandlebars/models/index.js
@@ -1,5 +1,16 @@
+const Todo = require('./Todo');
 const User = require('./User');
 
+User.hasMany(Todo, {
+	foreignKey: 'userId',
+	onDelete: 'CASCADE',
+});
+
+Todo.belongsTo(User, {
+	foreignKey: 'userId'
+});
+
 module.exports = {
-    User,
-};
+	Todo,
+	User, 
+}
