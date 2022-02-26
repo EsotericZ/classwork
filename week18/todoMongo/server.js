7325a4fd933b1b576c13a7603623124a967d941f
EsotericZ <cjsand03@gmail.com>
2022-02-26 12:07:19 -0700

    added stuff

--- week18/todoMongo/server.js
+++ week18/todoMongo/server.js
@@ -1,19 +1,19 @@
+const express = require('express');
 const mongoose = require('mongoose');
-const User = require('./model/User');
-const Todo = require('./model/Todo');
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
 
 mongoose.connect('mongodb://localhost:27017/todoMongoDB')
     .then(async () => {
         console.log('Successfully Connected to MongoDB');
-        const newUser = await User.create({
-            username: 'XRP FTW',
-            role: 'Employee',
-            powerLevel: 9001,
-        });
-        const newTodo = await Todo.create({
-            text: 'Eat Cake',
-        })
-        console.log(newUser)
-        console.log(newTodo)
     })
     .catch(err => console.log(err));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+app.listen(PORT, () => console.log('Server is Running'));
